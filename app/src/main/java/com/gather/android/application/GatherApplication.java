cc7378f95d89333c5eade80eab33c22b8382a8ed
Christain <495419499@qq.com>
2015-04-03 17:44:54 +0800

    4.3

--- app/src/main/java/com/gather/android/application/GatherApplication.java
+++ app/src/main/java/com/gather/android/application/GatherApplication.java
@@ -58,7 +58,6 @@ public class GatherApplication extends FrontiaApplication {
 			StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectAll().penaltyDialog().build());
 			StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll().penaltyDeath().build());
 		}
-
 		initVolley();
 		initImageLoader(getApplicationContext());
 
